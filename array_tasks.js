489ea13570cb15edf68b50f6c3f8e1e3cfe98c11
Mark Blanford <markytorture@hotmail.com>
2018-02-14 16:57:12 +0000

    first two tests passed

---
+++ array_tasks.js
@@ -0,0 +1,47 @@
+const arrayTasks = {
+
+	concat: function(arr1, arr2){
+    const concatenatedArray = [];
+    arr1.forEach(function(item){
+      concatenatedArray.push(item);
+    });
+    arr2.forEach(function(item){
+      concatenatedArray.push(item);
+    })
+    return concatenatedArray;
+	},
+
+	insertAt: function(arr, itemToAdd, index){
+    arr.splice(index, 0, itemToAdd);
+    return arr;
+	},
+
+	// square: function (arr) {
+
+	// },
+
+	// sum: function (arr) {
+
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+
+	// }
+
+	// ----------- EXTENSION ------------
+
+	// findDuplicates: function (arr) {
+
+	// },
+
+}
+
+module.exports = arrayTasks
